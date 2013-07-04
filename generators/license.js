cfd5f7a72c0c6a4a6d0147e4f7ec6fdd05b8c9ae
Austin Morton <amorton@juvsoft.com>
2013-07-04 00:33:44 -0400

    Update license generator close #28

--- generators/license.js
+++ generators/license.js
@@ -1,6 +1,6 @@
 // ==========================================================================
 // Project:   Ember Data Django Rest Adapter
-// Copyright: 
-// License:   
+// Copyright: (c) 2013 Toran Billups http://toranbillups.com
+// License:   MIT
 // ==========================================================================
 
