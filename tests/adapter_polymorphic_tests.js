ef9376fd0daa31277d721702c140b2b7b2b9017a
Paul Dufour <paul@brit.co>
2014-05-28 18:14:50 -0700

    update test files

--- tests/adapter_polymorphic_tests.js
+++ tests/adapter_polymorphic_tests.js
@@ -11,54 +11,144 @@ module('polymorphic integration tests', {
     }
 });
 
-// test('ajax response with array of polymorphic records renders hasMany correctly', function() {
-// 		var json = {
-// 			"id": 1,
-// 			"username": "Paul",
-// 			"messages": [
-// 				{
-// 				"id": 2,
-// 				"type": "Post",
-// 				"content": "Foo"
-// 				},
-// 				{
-// 				"id": 3,
-// 				"type": "Comment",
-// 				"content": "Bar"
-// 				}
-// 			]
-// 		};
+asyncTest('test polymorphic hasMany', function() {
+    var json = {
+        "id": 100,
+        "username": "Paul",
+        "messages": [
+            {
+                "id": 200,
+                "type": "Post",
+            },
+            {
+                "id": 300,
+                "type": "Comment",
+            }
+        ]
+    };
 
-//     stubEndpointForHttpRequest('/api/users/1/', json);
-//     visit("/messages").then(function() {
-//       var count = Ember.$("#messages li").length;
-//       equal(count, json.messages.length, "count was instead: " + count);
+    stubEndpointForHttpRequest('/api/users/1/', json);
 
-//       var messageContents = [];
+    Ember.run(App, function(){
+        // App.store = DS.Store.extend();
+        // var store = TestStore.create();
+        // console.log(store);
+        var store = App.__container__.lookup('store:main');
 
-//       Ember.$("#messages li").each(function() {
-//       	messageContents.push(Ember.$.trim($(this).text()));
-//       });
+        store.find('user', 1).then(function(user) {
+            messages = user.get('messages').toArray();
+            equal(messages.length, 2);
+            console.log('3');
+            store.unloadAll();
+            start();
+        });
+    });
+});
 
-//       equal(messageContents[0], 'Foo');
-//       equal(messageContents[1], 'Bar');
+asyncTest('test async polymorphic hasMany', function() {
+    App.User.reopen({
+        messages: DS.hasMany('message', { polymorphic: true, async: true })
+    });
+
+	var json = {
+		"id": 1,
+		"username": "Paul",
+		"messages": [
+			{
+    			"id": 2,
+    			"type": "Post",
+			},
+			{
+    			"id": 3,
+    			"type": "Comment",
+			}
+		]
+	};
+
+    stubEndpointForHttpRequest('/api/users/1/', json);
+
+    Ember.run(App, function(){
+        var store = App.__container__.lookup('store:main');
+
+        store.find('user', 1).then(function(user) {
+            equal(user.get('id'),1);
+            console.log('1',user.get('messages'));
+            user.get('messages').then(function(messages) {
+                console.log('2');
+                equal(messages.toArray().length, 2);
+
+                // Reset to async false
+                App.User.reopen({
+                    messages: DS.hasMany('message', { polymorphic: true })
+                });
+
+                start();
+            });
+        });
+    });
+});
+
+
+// asyncTest('test async polymorphic belongsTo', function() {
+// 	var message_json = {
+// 		"id": 1,
+// 		"content": "yo yo yo",
+//         "author": 2,
+//         "authorType": "company"
+// 	};
+
+//     var company_json = {
+//         "id": 2,
+//         "name": "Big corp"
+//     }
+
+//     stubEndpointForHttpRequest('/api/messages/1/', message_json);
+//     stubEndpointForHttpRequest('/api/companies/2/', company_json);
+
+//     Ember.run(App, function(){
+//         var store = App.__container__.lookup('store:main');
+
+//         store.find('message', 1).then(function(message) {
+//             equal(message.get('content'),message_json.content);
+
+//             message.get('author').then(function(author) {
+//                 equal(author.get('name'),company_json.name);
+
+//                 // tell QUnit to run tests again
+//                 start();
+//             });
+
+//         });
 //     });
 // });
 
+// asyncTest('test sideloaded async polymorphic belongsTo', function() {
+//     var message_json = {
+//         "id": 1,
+//         "content": "yo yo yo",
+//         "authorType": "company",
+//         "author": {
+//             "id": 2,
+//             "name": "big corp"
+//         }
+//     };
 
-test('ajax response with polymorphic record renders belongsTo correctly', function() {
-		var json = {
-			"id": 1,
-			"content": "yo yo yo",
-			"author": {
-				"type": "company",
-				"name": "hi",
-				"id": 2
-			}
-		};
+//     stubEndpointForHttpRequest('/api/messages/1/', message_json);
 
-    stubEndpointForHttpRequest('/api/messages/1/', json);
-    visit("/message/1").then(function() {
-    	console.log(Ember.$('#message-user').text());
-    });
-});
+//     Ember.run(App, function(){
+//         var store = App.__container__.lookup('store:main');
+
+//         store.find('message', 1).then(function(message) {
+//             console.log('message',message);
+//             equal(message.get('content'),message_json.content);
+
+//             message.get('author').then(function(author) {
+//                 equal(author.get('name'),message_json.author.name);
+
+//                 // tell QUnit to run tests again
+//                 start();
+//             });
+
+//         });
+//     });
+// });
