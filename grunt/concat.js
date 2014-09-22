6352235b28861967421c53e4bd62df5f86d46a9b
Ben Konrath <ben@bagu.org>
2014-09-22 13:29:40 +0200

    Update to ember-data 1.0.0 beta.10 / ember 1.7.0.

--- grunt/concat.js
+++ grunt/concat.js
@@ -3,8 +3,8 @@ module.exports = {
     src: [
       'tests/lib/jquery-1.9.1.js',
       'tests/lib/handlebars-v1.2.1.js',
-      'tests/lib/ember.js',
-      'tests/lib/ember-data.js',
+      'tests/lib/ember-1.7.0.js',
+      'tests/lib/ember-data-1.0.0-beta.10.js',
       'tests/lib/jquery.mockjax.js',
       'tests/lib/tmpl.min.js',
       'dist/ember-data-django-rest-adapter.js',
